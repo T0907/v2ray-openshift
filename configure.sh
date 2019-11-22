3eade74f6c19f730dfdc90ac1a15464ce2c6ff6e
jaskon139 <jaskon139@gmail.com>
2019-11-23 02:01:03 +0800

    sd

--- configure.sh
+++ configure.sh
@@ -9,7 +9,7 @@ echo "------------"
 /kcptunserver 10.241.62.73 9999 $resultip $resultip 3824 &
 
 #run kcp
-#/server_linux_amd64 -t 127.0.0.1:8080 -l :3824 --mode fast2&
+/server_linux_amd64 -t 127.0.0.1:8080 -l :3824 --mode fast2&
 
 # Run V2Ray
 /usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
