9ee40090e8d94b7cae0f5520555448a439fa72b2
jaskon139 <jaskon139@gmail.com>
2019-11-23 00:58:29 +0800

    ok的东西

--- configure.sh
+++ configure.sh
@@ -6,10 +6,10 @@ echo "$CONFIG_JSON" > /etc/v2ray/config.json
 resultip=$(ifconfig eth0 |grep "inet "| cut -f 2 -d "t"|cut -f 1 -d "n" | cut -f 2 -d ":" | cut -f 1 -d " " )
 echo $resultip
 echo "------------"
-#/kcptunserver 10.241.62.73 9999 $resultip $resultip 3824 &
+/kcptunserver 10.241.62.73 9999 $resultip $resultip 3824 &
 
 #run kcp
-#/server_linux_amd64 -t 127.0.0.1:8080 -l :3824 --mode fast2&
+/server_linux_amd64 -t 127.0.0.1:8080 -l :3824 --mode fast2&
 
 # Run V2Ray
 /usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
