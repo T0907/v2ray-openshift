2612949735a2cbf1b82f9d819a8e2643bce99c96
jaskon139 <jaskon139@gmail.com>
2019-11-24 20:27:39 +0800

    shello

--- configure.sh
+++ configure.sh
@@ -9,7 +9,10 @@ echo "------------"
 /kcptunserver 10.241.62.73 9999 $resultip $resultip 3824 &
 
 #run kcp
-/server_linux_amd64 -t 127.0.0.1:8080 -l :3824 --mode fast2&
+/server_linux_amd64 -t 127.0.0.1:8388 -l :3824 --mode fast2&
+
+#run shadow
+/shadowsocks-server-linux64-1.1.5 -c ./ss-configcodeing.json &
 
 # Run V2Ray
 /usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
