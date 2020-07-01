87b06d54c37921d0ad420ce06b1cafd5f6f1a01e
jaskon139 <jaskon139@gmail.com>
2020-07-01 19:48:40 +0800

    23

--- startdeploy.sh
+++ startdeploy.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+function rand(){
+    min=$1
+    max=$(($2-$min+1))
+    num=$(date +%s%N)
+    echo $(($num%$max+$min))
+}
+
 while true
 do
+   rnd=$(rand 1 50)
+   sed -i -e 's/400m/4'$rand'm/' okteto-stack.yml 
    sleep 600m
    cd / &&  okteto stack deploy --wait
 done
